d87d92ff7f9288bb6f36ac5e49dca1b771891803
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-03-02 21:30:36 -0300

    chore: config database connection

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 
 import { app } from "./app";
 
+import "./database";
+
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server is running on port: ${process.env.PORT}`);
 });
