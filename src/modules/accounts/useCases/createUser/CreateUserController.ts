c2cc1526ebe1dd0969a12c04798d45ce7d58c70f
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-03-04 19:47:38 -0300

    refactor: refact error handling

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -6,17 +6,13 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController implements IController {
   async handle(request: Request, response: Response): Promise<Response> {
-    try {
-      const { email, password } = request.body;
+    const { email, password } = request.body;
 
-      const createUserUseCase = container.resolve(CreateUserUseCase);
+    const createUserUseCase = container.resolve(CreateUserUseCase);
 
-      const user = await createUserUseCase.execute({ email, password });
+    const user = await createUserUseCase.execute({ email, password });
 
-      return response.status(201).json(user);
-    } catch (error) {
-      return response.status(error.statusCode).json({ message: error.message });
-    }
+    return response.status(201).json(user);
   }
 }
 
