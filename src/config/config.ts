aa82f2909f20cf716bcf42609354cb7cfb420ef4
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-03-05 14:11:17 -0300

    feat: add Client sign in

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
 const jwtUserSecret = "373c2b1ed206e654fd9945bb54ae50f0";
+const jwtClientSecret = "a49c6ab2b1f3df3cf3b00196549ebdde";
 
-export { jwtUserSecret };
+export { jwtUserSecret, jwtClientSecret };
