3598f83af056a23d96bd1d3f1ac9fd965f204bf9
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-03-08 21:30:04 -0300

    feat: add client update and delete feature

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -4,8 +4,10 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureClient } from "../middlewares/ensureClient";
 import { ensureUser } from "../middlewares/ensureUser";
 import { CreateClientController } from "../modules/accounts/useCases/createClient/CreateClientController";
+import { DeleteClientController } from "../modules/accounts/useCases/deleteClient/DeleteClientController";
 import { ListClientsController } from "../modules/accounts/useCases/listClients/ListClientsController";
 import { ShowClientProfileController } from "../modules/accounts/useCases/showClientProfile/ShowClientProfileController";
+import { UpdateClientProfileController } from "../modules/accounts/useCases/UpdateClientProfile/UpdateClientProfileController";
 import { ListClientOrdersController } from "../modules/orders/useCases/listClientOrders/ListClientOrdersController";
 
 const routes = Router();
@@ -14,6 +16,8 @@ const createClientController = new CreateClientController();
 const showClientProfileController = new ShowClientProfileController();
 const listClientsController = new ListClientsController();
 const listClientOrdersController = new ListClientOrdersController();
+const updateClientProfileController = new UpdateClientProfileController();
+const deleteClientController = new DeleteClientController();
 
 routes.post("/", createClientController.handle);
 routes.get("/", ensureAuthenticated, ensureUser, listClientsController.handle);
@@ -28,6 +32,18 @@ routes.get(
   ensureAuthenticated,
   ensureClient,
   listClientOrdersController.handle
+);
+routes.put(
+  "/",
+  ensureAuthenticated,
+  ensureClient,
+  updateClientProfileController.handle
+);
+routes.delete(
+  "/",
+  ensureAuthenticated,
+  ensureClient,
+  deleteClientController.handle
 );
 
 export { routes as ClientsRoutes };
